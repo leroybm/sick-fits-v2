ae71c5127bc3550fa1424512e9e675010bf9f048
Leroy <lelo.drax@gmail.com>
2021-02-10 22:52:36 -0300

    feat: render styles on server

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en-CA">
